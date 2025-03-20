8e8782e1584dc18d92b653cc1e4a1f3b83084c97
markov-ngz <batuque77mlh@gmail.com>
2025-03-20 21:03:39 +0100

    ADD spring boot code

---
+++ apps/invoiceReader/src/test/java/com/markov/invoiceReader/InvoiceReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markov.invoiceReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
