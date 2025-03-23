87e4b382e738b5ff5303b23e6c3b4479e9153b7f
markov-ngz <batuque77mlh@gmail.com>
2025-03-23 14:48:09 +0100

    ADD S3 User Object

--- apps/invoiceReader/src/main/java/invoiceReader/Handler.java
+++ apps/invoiceReader/src/main/java/invoiceReader/Handler.java
@@ -2,18 +2,19 @@ package invoiceReader;
 
 import java.util.List;
 
+import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.databind.ObjectMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-
-import software.amazon.awssdk.eventnotifications.s3.model.S3;
-import software.amazon.awssdk.eventnotifications.s3.model.S3EventNotification ;
-import software.amazon.awssdk.eventnotifications.s3.model.S3EventNotificationRecord;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
 import software.amazon.awssdk.regions.Region;
-
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 import software.amazon.awssdk.services.textract.TextractClient;
+import software.amazon.awssdk.services.textract.model.Block;
 import software.amazon.awssdk.services.textract.model.DetectDocumentTextRequest;
 import software.amazon.awssdk.services.textract.model.DetectDocumentTextResponse;
 import software.amazon.awssdk.services.textract.model.Document;
@@ -22,7 +23,16 @@ import software.amazon.awssdk.services.textract.model.S3Object ;
 
 public class Handler implements RequestHandler<SQSEvent, String>{
 
-    TextractClient textractClient ; 
+    private TextractClient textractClient ; 
+
+    LambdaLogger logger ; 
+
+    SqsClient sqsClient ; 
+
+    String queueUrl  = "https://sqs.eu-west-3.amazonaws.com/<project_id>/<name"; 
+
+    ObjectMapper objectMapper = new ObjectMapper() ; 
+
 
     @Override
     public String handleRequest(SQSEvent event, Context context)
@@ -30,7 +40,7 @@ public class Handler implements RequestHandler<SQSEvent, String>{
 
       Region region = Region.EU_WEST_3 ; 
 
-      LambdaLogger logger = context.getLogger();
+      this.logger = context.getLogger();
       
       logger.log("EVENT TYPE: " + event.getClass());
 
@@ -39,42 +49,68 @@ public class Handler implements RequestHandler<SQSEvent, String>{
               .region(region)
               .build();
 
-      event.getRecords().forEach( m -> processSQSInvoiceEvent(m.getBody())) ; 
+      this.sqsClient = SqsClient.create() ; 
+      
 
-      return  "Hello World" ; 
-    }
+      event.getRecords().forEach( m -> processSQSMessage(m)) ; 
 
-    private void processSQSInvoiceEvent(String sqsEventBody){
-      S3EventNotification s3EventNotification = S3EventNotification.fromJson(sqsEventBody) ; 
-      List<S3EventNotificationRecord> records = s3EventNotification.getRecords();
-      records.forEach( record -> processS3InvoiceEvent(record) );
+      return  "return data do not matter" ;
       
     }
 
-    private void processS3InvoiceEvent(S3EventNotificationRecord s3EventNotificationRecord){
+    /*
+    * Process SQSMessage with a message body like S3UserObject
+    * 1. Parse SQSMessageBody to  S3UserObjectwithText
+    * 2. Extract Text from S3Object
+    * 3. Write message to queue
+    */
+    private void processSQSMessage(SQSMessage sqsMessage){
+        this.logger.log(sqsMessage.getBody());
+        
+        try {
 
-      S3 s3 = s3EventNotificationRecord.getS3() ; 
+          // 1. Parse Body 
+          S3UserObjectwithText s3UserObject = (S3UserObjectwithText) S3UserObject.fromSQSMessage(sqsMessage) ;  
 
+
+          // 2. Extract Text 
+          List<Block> blocks = extractText(s3UserObject.getBucketName(), s3UserObject.getObjectKey()) ;
+
+          s3UserObject.setBlocks(blocks);
+
+          // 3. Write message 
+          String messageBody = this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(s3UserObject) ; 
+
+          sqsClient.sendMessage(SendMessageRequest.builder()
+          .queueUrl(queueUrl)
+          .messageBody(messageBody)
+          .delaySeconds(10)
+          .build());
+
+        } catch (Exception e) {
+          logger.log(e.getMessage(),LogLevel.ERROR) ;
+        }
+
+    }
+    
+    private List<Block> extractText(String bucketName, String objectKey){
+      
       S3Object textractS3Object = S3Object.builder()
-      .bucket(s3.getBucket().getName())
-      .name(s3.getObject().getKey())
+      .bucket(bucketName)
+      .name(objectKey)
       .build();
 
       Document document = Document.builder()
       .s3Object(textractS3Object)
       .build();
 
-      // DetectDocumentTextRequest detectDocumentTextRequest = DetectDocumentTextRequest.builder()
-      // .document(document)
-      // .build();
+      DetectDocumentTextRequest detectDocumentTextRequest = DetectDocumentTextRequest.builder()
+      .document(document)
+      .build();
 
-      // DetectDocumentTextResponse textResponse = this.textractClient.an;
+      DetectDocumentTextResponse textResponse = this.textractClient.detectDocumentText(detectDocumentTextRequest);
 
-      
+      return textResponse.blocks() ; 
     }
 
-
 }
-// https://docs.aws.amazon.com/fr_fr/sdk-for-java/latest/developer-guide/examples-s3-event-notifications.html 
-// use textract 
-// https://docs.aws.amazon.com/fr_fr/sdk-for-java/latest/developer-guide/java_textract_code_examples.html 
---
+++ apps/invoiceReader/src/main/java/invoiceReader/S3UserObject.java
@@ -0,0 +1,48 @@
+package invoiceReader;
+
+import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.databind.ObjectMapper;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+
+public class S3UserObject {
+    
+    private String bucketName; 
+    private String objectKey;
+    private int userId ;
+    
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getObjectKey() {
+        return objectKey;
+    }
+
+    public void setObjectKey(String objectKey) {
+        this.objectKey = objectKey;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public static S3UserObject fromSQSMessage(SQSMessage sqsMessage)throws Exception{
+        
+        ObjectMapper objectMapper = new ObjectMapper() ; 
+        
+        String body = sqsMessage.getBody() ; 
+
+        S3UserObject newS3UserFile =  objectMapper.readValue(body, S3UserObject.class) ; 
+
+        return newS3UserFile ; 
+
+    }
+}
