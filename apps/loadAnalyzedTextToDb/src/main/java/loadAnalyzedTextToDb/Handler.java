477a5aa9d532906e7410b471f53514bd18624d99
markov-ngz <batuque77mlh@gmail.com>
2025-03-27 13:26:33 +0100

    ADD message body rertrieval

--- apps/loadAnalyzedTextToDb/src/main/java/loadAnalyzedTextToDb/Handler.java
+++ apps/loadAnalyzedTextToDb/src/main/java/loadAnalyzedTextToDb/Handler.java
@@ -8,18 +8,40 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 
+import analyzeDocument.S3UserObject;
+
 public class Handler  implements RequestHandler<SQSEvent, String>{
 
     private static final Region DEFAULT_REGION = Region.EU_WEST_3;
     private  String QUEUE_URL ;
+    private LambdaLogger logger;
 
     @Override
     public String handleRequest(SQSEvent event, Context context) {
         
+        this.logger = context.getLogger();
+
         // 1. Extract Message content
+        for(SQSMessage message : event.getRecords()){
+
+            try {
+                processMessage(message) ; 
+            } catch (Exception e) {
+                logger.log(e.getMessage() , LogLevel.ERROR) ; 
+            }
+        
+        }
 
         // 2. Write to Database with userId 
         
         return null ; 
+    }
+
+    private void processMessage(SQSMessage message) throws Exception{
+        
+        logger.log("Starting processing Message with Id :" + message.getMessageId(),LogLevel.INFO) ; 
+        
+        String body = message.getBody() ; 
+        
     }
 }
