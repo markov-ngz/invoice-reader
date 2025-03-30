d2a9f77562e6fe41c6fb0a34c01e69546f5eb8d8
markov-ngz <batuque77mlh@gmail.com>
2025-03-30 10:50:13 +0200

    ADD invoice entities

--- apps/InvoiceService/src/main/java/com/invoiceReader/InvoiceService/entities/Invoice.java
+++ apps/InvoiceService/src/main/java/com/invoiceReader/InvoiceService/entities/Invoice.java
@@ -1,5 +1,7 @@
 package com.invoiceReader.InvoiceService.entities ; 
 
+import java.sql.Date;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -10,11 +12,26 @@ import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
+@Data
 public class Invoice {
     
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id ;
 
-    private String name ; 
+    private String invoiceNumber;
+    private Date invoiceDate;
+    
+    private String supplier ; // Of course Id would be better with real links and entities but this is done as a POC for OCR not to create an ERP from scratch 
+    private String supplierAddress ;
+    
+    
+    private String customerName;
+    private String customerAddress;
+
+
+    @OneToMany
+    private List<InvoiceLine> items;
+    
+    private double totalAmount;
 }
---
+++ apps/InvoiceService/src/main/java/com/invoiceReader/InvoiceService/entities/InvoiceLine.java
@@ -0,0 +1,16 @@
+package com.invoiceReader.InvoiceService.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class InvoiceLine {
+
+    private String description;
+    private int quantity;
+    private double unitPrice;
+    private String tax  ;
+    private double totalAmount ; 
+
+}
