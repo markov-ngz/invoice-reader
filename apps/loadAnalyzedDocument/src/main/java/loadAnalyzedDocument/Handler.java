e0fa154d9934e016a9758f7422c21434467ecc9b
markov-ngz <batuque77mlh@gmail.com>
2025-04-04 08:50:35 +0200

    ADD simple other logging implementation

--- apps/loadAnalyzedDocument/src/main/java/loadAnalyzedDocument/Handler.java
+++ apps/loadAnalyzedDocument/src/main/java/loadAnalyzedDocument/Handler.java
@@ -14,68 +14,89 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import com.amazonaws.lambda.thirdparty.com.fasterxml.jackson.databind.ObjectMapper;
 
+public class Handler implements RequestHandler<Object, String>{
 
-public class Handler  implements RequestHandler<SQSEvent, String>{
 
-    private LambdaLogger logger;
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
+    private Logger logger ;
+  
     @Override
-    public String handleRequest(SQSEvent event, Context context) {
+    public String handleRequest(Object event, Context context)
+    {
+      this.logger = LoggerFactory.getLogger(Handler.class) ; 
+      logger.info("Example log from {}", event.getClass());
+  
+      logger.error("Fake error sorry i just pressed the wrong button");
+  
+      return "Dzien dobry";
+    }
+  }
+  
 
-        // 0. Setup 
+// public class Handler  implements RequestHandler<SQSEvent, String>{
 
-        // 0.1 Logger
-        this.logger = context.getLogger();
+//     private LambdaLogger logger;
+//     private final ObjectMapper objectMapper = new ObjectMapper();
+
+//     @Override
+//     public String handleRequest(SQSEvent event, Context context) {
+
+//         // 0. Setup 
+
+//         // 0.1 Logger
+//         this.logger = context.getLogger();
         
-        for(SQSMessage message : event.getRecords()){
+//         for(SQSMessage message : event.getRecords()){
 
-            logger.log("Starting processing Message with Id :" + message.getMessageId(),LogLevel.INFO) ; 
+//             logger.log("Starting processing Message with Id :" + message.getMessageId(),LogLevel.INFO) ; 
             
-            try {    
+//             try {    
         
-                // 1. Extract Message content
-                AnalyzedInvoiceDTO analyzedDocument =  parseJson(message.getBody(), AnalyzedInvoiceDTO.class, objectMapper) ;
+//                 // 1. Extract Message content
+//                 AnalyzedInvoiceDTO analyzedDocument =  parseJson(message.getBody(), AnalyzedInvoiceDTO.class, objectMapper) ;
         
-                logger.log("Succesfully parsed Message with Id :" + message.getMessageId(),LogLevel.INFO) ; 
+//                 logger.log("Succesfully parsed Message with Id :" + message.getMessageId(),LogLevel.INFO) ; 
         
-                // 2. Write Value to API 
-                // TODO 
+//                 // 2. Write Value to API 
+//                 // TODO 
 
-                logger.log("Successfully processed message with Id" + message.getMessageId(),LogLevel.INFO) ; 
+//                 logger.log("Successfully processed message with Id" + message.getMessageId(),LogLevel.INFO) ; 
 
-            } catch (Exception e) {
-                logger.log("Failed processed message with Id" + message.getMessageId() + " with err " + e.getMessage() ,LogLevel.ERROR) ; 
-            }
+//             } catch (Exception e) {
+//                 logger.log("Failed processed message with Id" + message.getMessageId() + " with err " + e.getMessage() ,LogLevel.ERROR) ; 
+//             }
         
-        }
+//         }
         
-        return null ; 
-    }
+//         return "null" ; 
+//     }
 
-    public static <T> T parseJson(String jsonMessage, Class<T> clazz, ObjectMapper objectMapper) {
+//     public static <T> T parseJson(String jsonMessage, Class<T> clazz, ObjectMapper objectMapper) {
         
-        try {
-            return objectMapper.readValue(jsonMessage, clazz);
-        } catch (Exception e) {
-            throw new RuntimeException("Error parsing JSON message", e);
-        }
-    }
+//         try {
+//             return objectMapper.readValue(jsonMessage, clazz);
+//         } catch (Exception e) {
+//             throw new RuntimeException("Error parsing JSON message", e);
+//         }
+//     }
 
 
-    // private Connection getConnection(DatabaseConfig dbConfig){
-    //     try {
-    //         return DriverManager.getConnection(
-    //             dbConfig.getUrl() , // "jdbc:postgresql://localhost:5432/yourdatabase"  
-    //             dbConfig.getUsername() ,  
-    //             dbConfig.getPassword()
-    //          );            
-    //     } catch (SQLException e) {
-    //         logger.log(e.getMessage(), LogLevel.ERROR);
-    //         return null ; 
-    //     }
+//     // private Connection getConnection(DatabaseConfig dbConfig){
+//     //     try {
+//     //         return DriverManager.getConnection(
+//     //             dbConfig.getUrl() , // "jdbc:postgresql://localhost:5432/yourdatabase"  
+//     //             dbConfig.getUsername() ,  
+//     //             dbConfig.getPassword()
+//     //          );            
+//     //     } catch (SQLException e) {
+//     //         logger.log(e.getMessage(), LogLevel.ERROR);
+//     //         return null ; 
+//     //     }
 
-    // }
-}
+//     // }
+// }
